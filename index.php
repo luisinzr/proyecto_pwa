3b0a37c1b4eaeae86efc192833a96bb40a032e49
luisinzr <126990770+luisinzr@users.noreply.github.com>
2025-07-18 18:48:03 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Bienvenido al Restaurante UBE</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/inicio-estilos.css" rel="stylesheet"> <!-- Enlace a tu nuevo CSS -->
+</head>
+<body>
+  <div class="container">
+    <img src="img/logo.gif" alt="Logo Restaurante" class="logo"><br> <!-- Logo animado -->
+    <h1 class="mb-4">🍽️ Bienvenido al Restaurante PROYECTO_F</h1>
+    <a href="vista/registro.php" class="btn btn-outline-primary btn-lg mb-3">📝 Registrarse</a><br>
+    <a href="vista/login.php" class="btn btn-success btn-lg">🔐 Iniciar Sesión</a>
+  </div>
+</body>
+</html>
+
